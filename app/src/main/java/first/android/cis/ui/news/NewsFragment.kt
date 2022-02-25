09372952a748ceab503023de2a7f1d2d702c212f
Andrey Brednikov <andrewlis4774@gmail.com>
2022-02-25 09:24:25 +0200

    CIS-26 <Test2>

--- app/src/main/java/first/android/cis/ui/news/NewsFragment.kt
+++ app/src/main/java/first/android/cis/ui/news/NewsFragment.kt
@@ -15,7 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import first.android.cis.R
 import first.android.cis.network.getNews.NewsRepository
 
-//CIS-26-тестовый-таск
+//CIS-26v2-тестовый-таск
 class NewsFragment : Fragment() {
     private val myAdapter by lazy{NewsAdapter()}
     private lateinit var viewModel: NewsViewModel
